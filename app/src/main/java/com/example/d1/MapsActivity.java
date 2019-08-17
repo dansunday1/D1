1c3e09ef6e4a7ae8c97cc5fb7b14c61f6e333a01
dansunday1 <dansunday1@gmail.com>
2019-08-16 23:20:58 -0400

    Fixed bugs

--- app/src/main/java/com/example/d1/MapsActivity.java
+++ app/src/main/java/com/example/d1/MapsActivity.java
@@ -19,6 +19,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
@@ -29,8 +30,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
+     * This is where we can add markers or lines, add listeners or move the camera.
+     * In this case, we just add a GC marker near Columbia, MD.
      * If Google Play services is not installed on the device, the user will be prompted to install
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
@@ -41,7 +42,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         // Add a marker in Sydney and move the camera
         LatLng columbia = new LatLng(35.2256, -76.9023);
-        mMap.addMarker(new MarkerOptions().position(columbia).title("Marker in Sydney"));
+        mMap.addMarker(new MarkerOptions()
+                .position(columbia)
+                .title("Marker in Columbia")
+                .draggable(true)
+        );
         mMap.moveCamera(CameraUpdateFactory.newLatLng(columbia));
     }
 }
