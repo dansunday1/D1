7b69ac85aefe7225ac20acec563182397e62ce02
dansunday1 <dansunday1@gmail.com>
2019-08-17 23:40:16 -0400

    Got map display working, and ask user for location access permission.

--- app/src/main/java/com/example/d1/MapsActivity.java
+++ app/src/main/java/com/example/d1/MapsActivity.java
@@ -1,8 +1,15 @@
 package com.example.d1;
 
-import androidx.fragment.app.FragmentActivity;
-
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.FragmentActivity;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -13,6 +20,8 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
+    private static final String TAG = "MapsActivity";
+    private static final int LOCATION_REQUEST_CODE = 101;
     private GoogleMap mMap;
 
     @Override
@@ -24,8 +33,64 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        setupPermissions();
     }
 
+    private void setupPermissions() {
+        int permission = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION);
+
+        // TODO: use SnackBar instead of AlertDialog
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            Log.i(TAG, "Permission to access location is denied");
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+                AlertDialog.Builder builder =
+                        new AlertDialog.Builder(this);
+                builder.setMessage("Permission to access location info is required for this app.")
+                        .setTitle("Permission required");
+
+                builder.setPositiveButton("OK",
+                        new DialogInterface.OnClickListener() {
+
+                            public void onClick(DialogInterface dialog, int id) {
+                                Log.i(TAG, "Clicked");
+                                makeRequest();
+                            }
+                        });
+
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            } else {
+                makeRequest();
+            }
+        }
+    }
+
+    protected void makeRequest() {
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_REQUEST_CODE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case LOCATION_REQUEST_CODE: {
+
+                if (grantResults.length == 0
+                    || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+
+                    Log.i(TAG, "Permission has been denied by user");
+                    // TODO: Explain needed, and Ask 1 more time before exiting
+
+                } else {
+                    Log.i(TAG, "Permission has been granted by user");
+                }
+            }
+        }
+    }
 
     /**
      * Manipulates the map once available.
@@ -39,14 +104,16 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        Log.i(TAG, "Map is ready");
 
-        // Add a marker in Sydney and move the camera
-        LatLng columbia = new LatLng(35.2256, -76.9023);
+        // Add a marker in Columbia MD, and move the camera
+        LatLng columbia = new LatLng(39.2256, -76.9023);
         mMap.addMarker(new MarkerOptions()
                 .position(columbia)
-                .title("GC Columbia MD")
+                .title("Columbia GC")
                 .draggable(true)
         );
         mMap.moveCamera(CameraUpdateFactory.newLatLng(columbia));
     }
+
 }
