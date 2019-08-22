1464ee2b2c8d1fbdf44ae2257cd89a1bcf79c786
dansunday1 <dansunday1@gmail.com>
2019-08-21 23:26:04 -0400

    Added initial device location code

--- app/src/main/java/com/example/d1/MapsActivity.java
+++ app/src/main/java/com/example/d1/MapsActivity.java
@@ -1,14 +1,15 @@
 package com.example.d1;
 
 import android.Manifest;
-import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
@@ -25,17 +26,19 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
 
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity
+        implements GoogleMap.OnMyLocationButtonClickListener,
+        GoogleMap.OnMyLocationClickListener,
+        OnMapReadyCallback {
 
     private static final String TAG = MapsActivity.class.getSimpleName();
     private static final int LOCATION_REQUEST_CODE = 101;
     private FusedLocationProviderClient fusedLocationClient;
     private Location mLastLocation;
-    private Location mCurrentLocation;
+    private Location mLocation;
     private GoogleMap mMap;
     private CameraPosition mCameraPosition;
 
-    @SuppressLint("MissingPermission")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,23 +48,37 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera.
+     * In this case, we just add a GC marker near Columbia, MD.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        Log.i(TAG, "Map is ready");
 
         // get permission to access fine location
         setupPermissions();
 
-/*
-        // TODO: 8/20/19   Test that location access permission is granted.
+        // TODO: 8/21/19 Test that fine location access permission is granted.
         if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION)
             != PackageManager.PERMISSION_GRANTED) {
+            Log.i(TAG, "checkSelfPermission failed");
             // TODO: Consider calling Activity#requestPermissions
             // here to request the missing permissions, and then overriding
             //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
             //                                          int[] grantResults)
             // to handle the case where the user grants the permission.
             // See the documentation for Activity#requestPermissions for more details.
-            return;
         }
-*/
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
         fusedLocationClient.getLastLocation()
                 .addOnSuccessListener(this, new OnSuccessListener<Location>() {
@@ -73,9 +90,69 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                         }
                     }
                 });
+
+        // enable my location
+        mMap.setMyLocationEnabled(true);
+        mMap.setOnMyLocationButtonClickListener(this);
+        mMap.setOnMyLocationClickListener(this);
+
+        // Get current device location
+        GPSTracker mGpsTracker = new GPSTracker(getApplicationContext());
+        mLocation = mGpsTracker.getLocation();
+        if (mLocation == null) {
+            mLocation = mLastLocation;
+            if (mLocation == null) {
+                Log.i(TAG, "mLocation cannot be found");
+                // TODO: 8/21/19 Give feedback to user re location not available
+                return;     // done
+            }
+        }
+        Log.i(TAG, "mLocation: "
+                +"lat= "+mLocation.getLatitude() +", lng= "+mLocation.getLongitude());
+        // move to initial location
+        LatLng here = new LatLng(mLocation.getLatitude(), mLocation.getLongitude());
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(here));
+
+        // TODO: 8/21/19 implement mLocation updates
+
+        // Add a marker for HG GC in Columbia MD
+        LatLng columbiaGC = new LatLng(39.225618, -76.9001157);
+        mMap.addMarker(new MarkerOptions()
+                .position(columbiaGC)
+                .title("Hobbits GC")
+                .draggable(false)
+        );
+
+
+        // TODO: 8/21/19 Mark ball location
+        // TODO: 8/21/19 Mark target pin location
+        // TODO: 8/21/19 Mark aimPoint (dragable)
+        // TODO: 8/21/19 Show lines and distances: ball->aimPt->pin
+
+        // TODO: 8/21/19 Show distance rings from ball toward target pin
+        
     }
 
+    @Override
+    public void onMyLocationClick(@NonNull Location location) {
+        Toast.makeText(this, "Current location:\n" + location, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public boolean onMyLocationButtonClick() {
+        Toast.makeText(this, "MyLocation button clicked", Toast.LENGTH_SHORT).show();
+        // Return false so that we don't consume the event and the default behavior still occurs
+        // (the camera animates to the user's current position).
+        return false;
+    }
+
+
+    /*
+    * Access Permission code.
+    * TODO: 8/21/19 Consider putting access permission code in separate utility class
+     */
     private void setupPermissions() {
+        Log.i(TAG, "setupPermissions");
         int permission = ContextCompat.checkSelfPermission(this,
                 Manifest.permission.ACCESS_FINE_LOCATION);
 
@@ -106,6 +183,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
     protected void makeRequest() {
+        Log.i(TAG, "makeRequest");
         ActivityCompat.requestPermissions(this,
                 new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_REQUEST_CODE);
     }
@@ -113,11 +191,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onRequestPermissionsResult(int requestCode,
                                            String permissions[], int[] grantResults) {
+        Log.i(TAG, "onRequestPermissionsResult= "+grantResults);
         switch (requestCode) {
             case LOCATION_REQUEST_CODE: {
 
                 if (grantResults.length == 0
-                    || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                        || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
 
                     Log.i(TAG, "User Denies Location Permission");
 
@@ -126,45 +205,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 }
             }
         }
-    }
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera.
-     * In this case, we just add a GC marker near Columbia, MD.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-        Log.i(TAG, "Map is ready");
-
-        // Add a marker in Columbia MD, and move the camera
-        LatLng columbiaGC = new LatLng(39.225618, -76.9001157);
-        mMap.addMarker(new MarkerOptions()
-                .position(columbiaGC)
-                .title("Columbia GC")
-                .draggable(false)
-        );
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(columbiaGC));
-
-        // Get current device location
-        GPSTracker mGpsTracker = new GPSTracker(getApplicationContext());
-        mCurrentLocation = mGpsTracker.getLocation();
-        if (mCurrentLocation == null) {
-            mCurrentLocation = mLastLocation;
-            if (mCurrentLocation == null) {
-                Log.i(TAG, "mCurrentLocation cannot be found");
-            }
-        }
-        Log.i(TAG, "mCurrentLocation: "
-                +"lat= "+mCurrentLocation.getLatitude()
-                +", lng= "+mCurrentLocation.getLongitude());
-
-        // TODO: 8/19/19 display current location marker on map 
     }
 
 }
